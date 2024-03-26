aa03b61e7ebc15927a7da9be9264f85a523f4e97
favour1818 <84434963+favour1818@users.noreply.github.com>
2024-03-26 18:57:05 +0100

    all bugs has been resolved and the stylings has been moved to an external css file

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const button = document.getElementById("horoscope-form");
 const textArea = document.getElementById("text-area");
 const displayArea = document.getElementById("display-area");
-let validHoroscopeString = "";
-let textInDisplayArea = "";
-const buttonstuff = document.getElementById("basic-button");
-
+const button = document.getElementById("submit-button");
+const form = document.getElementById("horoscope-form");
 const horoscopes = [
   "aquarius",
   "capricorn",
@@ -21,67 +18,77 @@ const horoscopes = [
   "ophiuchus",
 ];
 
-// this function checks if the button is equal to the avaliable values
-function buttonclick() {
+function handleClick() {
   // to turn any valid values into comparable with our valid Horoscope Strings
+  let textValues = `${textArea.value}`;
+  let validHoroscopeString = textValues.toLowerCase();
+  let ishoroscopeTrue = false;
 
-  let values = `${textArea.value}`;
-  validHoroscopeString = values.toLowerCase();
+  let textInDisplayArea = "";
+
+  // to return the valid messages for each of the horoscope signs
+
+  function getDisplayMessage() {
+    switch ((validHoroscopeString, ishoroscopeTrue)) {
+      case ishoroscopeTrue === true && validHoroscopeString === `aquarius`:
+        textInDisplayArea =
+          "You should be very proud of you acheivements sofar";
+        break;
+      case ishoroscopeTrue === true && validHoroscopeString === "capricon":
+        textInDisplayArea = `You  are skilled at navigating both the material and emotional re`;
+        break;
+      case ishoroscopeTrue === true && validHoroscopeString === "virgo":
+        textInDisplayArea = `You're Bold and Ambitious.You dive headfirst into even the most challenging situations and make sure you come out on top`;
+        break;
+      case ishoroscopeTrue === true && validHoroscopeString === "cancer":
+        console.log("the problem is not the switch");
+        textInDisplayArea = `You're higbreak;ly intuitive and manifest your psychic abilities in tangible spaces`;
+        break;
+      case ishoroscopeTrue === true && validHoroscopeString === "gemini":
+        textInDisplayArea = `You're spontaneous, playful, and adorably erratic. you are driven by you insatiable curiosity.`;
+        break;
+      case ishoroscopeTrue === true && validHoroscopeString === "leo":
+        textInDisplayArea = `You are delighted to embrace your royal status and you love to bask in the spotlight and celeberate yourself`;
+        break;
+      case ishoroscopeTrue === true && validHoroscopeString === "libra":
+        textInDisplayArea = `You're obsessed with symmetry and strive to create equilibrium in all areas of life - especially when it comes to matters of the heart`;
+        break;
+      case ishoroscopeTrue === true && validHoroscopeString === "sagittarius":
+        textInDisplayArea = `You are always on a quest for knowledg, chasing after geographical, intellectual and spiritual adventures`;
+        break;
+      case ishoroscopeTrue === true && validHoroscopeString === "scorpio":
+        textInDisplayArea = `You use emotional energy as fuel, cultivating powerful wisdom through both the physical and unseen realms.`;
+        break;
+      case ishoroscopeTrue === true && validHoroscopeString === "taurus":
+        textInDisplayArea = `You enjoy relaxing in serene, bucolic environments surrounded by soft sounds, soothing aromas, and succulent flavors`;
+        break;
+      case ishoroscopeTrue === true && validHoroscopeString === "aries":
+        textInDisplayArea = `You're Bold and Ambitious.You dive headfirst into even the most challenging situations and make sure you come out on top`;
+        break;
+      case ishoroscopeTrue === true && validHoroscopeString === "leo":
+        textInDisplayArea = `You're Bold and Ambitious.You dive headfirst into even the most challenging situations and make sure you come out on top`;
+        break;
+      case ishoroscopeTrue === true && validHoroscopeString === "pisces":
+        textInDisplayArea = `You're Bold and Ambitious.You dive headfirst into even the most challenging situations and make sure you come out on top`;
+        break;
+      case ishoroscopeTrue === true && validHoroscopeString === "ophiuchus":
+        textInDisplayArea = `You're Bold and Ambitious.You dive headfirst into even the most challenging situations and make sure you come out on top`;
+        break;
+      default:
+        console.log("the getdisplaly message switch is not working");
+        break;
+    }
+    displayArea.innerText = textInDisplayArea;
+  }
+
+  //to check if the text input is a valid horoscope and displays the adequate message
 
   !horoscopes.includes(validHoroscopeString)
-    ? console.log(`sorry ${values} is not a valid horoscope`)
-    : (console.log(`Your horoscope is ${values}`), console.log(values));
-  // to return the valid messages for each of the horoscope signs
-  switch (validHoroscopeString) {
-    case `${validHoroscopeString}` === `aquarius`:
-      textInDisplayArea = "You should be very proud of you acheivements sofar";
-      break;
-    case validHoroscopeString === "capricon":
-      textInDisplayArea = `You  are skilled at navigating both the material and emotional re`;
-      break;
-    case validHoroscopeString === "virgo":
-      textInDisplayArea = `You're Bold and Ambitious.You dive headfirst into even the most challenging situations and make sure you come out on top`;
-      break;
-    case validHoroscopeString === "cancer":
-      console.log("the problem is not the switch");
-      textInDisplayArea = `You're highly intuitive and manifest your psychic abilities in tangible spaces`;
-      break;
-    case validHoroscopeString === "gemini":
-      textInDisplayArea = `You're spontaneous, playful, and adorably erratic. you are driven by you insatiable curiosity.`;
-      break;
-    case validHoroscopeString === "leo":
-      textInDisplayArea = `You are delighted to embrace your royal status and you love to bask in the spotlight and celeberate yourself`;
-      break;
-    case validHoroscopeString === "libra":
-      textInDisplayArea = `You're obsessed with symmetry and strive to create equilibrium in all areas of life - especially when it comes to matters of the heart`;
-      break;
-    case validHoroscopeString === "sagittarius":
-      textInDisplayArea = `You are always on a quest for knowledg, chasing after geographical, intellectual and spiritual adventures`;
-      break;
-    case validHoroscopeString === "scorpio":
-      textInDisplayArea = `You use emotional energy as fuel, cultivating powerful wisdom through both the physical and unseen realms.`;
-      break;
-    case validHoroscopeString === "taurus":
-      textInDisplayArea = `You enjoy relaxing in serene, bucolic environments surrounded by soft sounds, soothing aromas, and succulent flavors`;
-      break;
-    case validHoroscopeString === "aries":
-      textInDisplayArea = `You're Bold and Ambitious.You dive headfirst into even the most challenging situations and make sure you come out on top`;
-      break;
-    case validHoroscopeString === "leo":
-      textInDisplayArea = `You're Bold and Ambitious.You dive headfirst into even the most challenging situations and make sure you come out on top`;
-      break;
-    case validHoroscopeString === "pisces":
-      textInDisplayArea = `You're Bold and Ambitious.You dive headfirst into even the most challenging situations and make sure you come out on top`;
-      break;
-    case validHoroscopeString === "ophiuchus":
-      textInDisplayArea = `You're Bold and Ambitious.You dive headfirst into even the most challenging situations and make sure you come out on top`;
-      break;
-    default:
-      textInDisplayArea = "not a valid text";
-      break;
-  }
-  displayArea.innerHTML = textInDisplayArea;
-  console.log(textInDisplayArea);
+    ? (displayArea.innerText = `sorry ${textValues} is not a valid horoscope`)
+    : ((ishoroscopeTrue = true),
+      console.log(textValues),
+      getDisplayMessage(ishoroscopeTrue, validHoroscopeString));
 }
 
-buttonstuff.addEventListener("click", buttonclick);
+button.addEventListener("click", handleClick);
+form.addEventListener("submit", handleClick);
